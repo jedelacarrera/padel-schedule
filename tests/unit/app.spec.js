a920ff43fe7babcbd51b2fb52eb4e7ba7315f723
jedelacarrera <jedelacarrera@uc.cl>
2020-09-12 19:52:14 -0300

    fix: :bug: :white_check_mark: tests

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { mount, createLocalVue } from '@vue/test-utils'
+import App from '@/App.vue'
+
+Vue.use(Vuetify)
+const localVue = createLocalVue()
+
+describe('App', () => {
+  const vuetify = new Vuetify()
+  const wrapper = mount(App, {
+    localVue,
+    vuetify,
+  })
+
+  it('Check App', () => {
+    expect(wrapper.find('img')).toBeTruthy()
+    expect(wrapper.find('HelloWorld')).toBeTruthy()
+  })
+})
