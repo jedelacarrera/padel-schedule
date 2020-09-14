fe9322ad55c4ecbf0db556944253a176c5e78cbf
jedelacarrera <jedelacarrera@uc.cl>
2020-09-14 00:01:09 -0300

    :bug: added init file

--- api/tests/test_map_date.py
+++ api/tests/test_map_date.py
@@ -3,4 +3,6 @@ from src.padel_requests import map_date
 
 def test_map_date():
     assert map_date("2020-11-12") == "12/11/2020"
+    assert map_date("2020.11.12") == "12/11/2020"
+    assert map_date("2020/11/12") == "12/11/2020"
     assert map_date("2020-1-2") == "02/01/2020"
