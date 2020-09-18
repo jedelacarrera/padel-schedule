9315aa41b5795c5740754ad501bface503e67bea
jedelacarrera <jedelacarrera@uc.cl>
2020-09-17 22:11:40 -0300

    :star: get conecta information (#6)
    * :star: get conecta information
    
    * :wrench: update cloud build
    
    * :star: map conecta reponse to useful data
    
    * :star: git ignore vscode

--- api/src/padel_requests/conecta.py
+++ api/src/padel_requests/conecta.py
@@ -22,6 +22,45 @@ COOKIES = {
 }
 
 
-def get_conecta_schedule(date):
+def get_conecta_schedule(date: str) -> dict:
     data = json.dumps({"idCuadro": "4", "fecha": date})  # 16/9/2020
-    return requests.post(URL, headers=HEADERS, cookies=COOKIES, data=data).json()
+    response = requests.post(URL, headers=HEADERS, cookies=COOKIES, data=data).json()
+    response = response["d"]
+    courts = [
+        get_info_from_court(court)
+        for court in response["Columnas"]
+        if court["TextoSecundario"] == "Pádel"
+    ]
+    return {
+        "initial_time": response["StrHoraInicio"],
+        "end_time": response["StrHoraFin"],
+        "name": response["Nombre"],
+        "courts": courts,
+    }
+
+
+def get_info_from_court(court: dict) -> dict:
+    bookings = [
+        {
+            "initial_time": booking["StrHoraInicio"],
+            "end_time": booking["StrHoraFin"],
+            "total_time": booking["Minutos"],
+        }
+        for booking in court["Ocupaciones"]
+    ]
+    bookings.sort(key=lambda x: x["initial_time"])
+    fixed_times = [
+        {
+            "initial_time": fixed_time["StrHoraInicio"],
+            "end_time": fixed_time["StrHoraFin"],
+            "total_time": fixed_time["Minutos"],
+            "valid": fixed_time["Clickable"],
+        }
+        for fixed_time in court["HorariosFijos"]
+    ]
+    fixed_times.sort(key=lambda x: x["initial_time"])
+    return {
+        "name": court["TextoPrincipal"],
+        "bookings": bookings,
+        "fixed_times": fixed_times,
+    }
--- front/src/components/tabs/ScheduleTab.vue
+++ front/src/components/tabs/ScheduleTab.vue
@@ -1,16 +1,68 @@
 <template>
   <div>
     <v-date-picker v-model="pickedDate" />
+    <div>{{ names }}. {{ initialTime }} - {{ finalTime }}</div>
+    <div>
+      {{ responses }}
+    </div>
   </div>
 </template>
 
 <script>
+const apiURL = 'https://padel-api-54td4ousva-uc.a.run.app/get_schedule'
 export default {
   data() {
     return {
       pickedDate: '',
+      responses: [],
     }
   },
-  methods: {},
+  computed: {
+    names() {
+      const names = this.responses.map(response => response.Nombre).join(', ')
+      return 'Clubes: ' + names
+    },
+    initialTime() {
+      let time = 12
+      this.responses.forEach(response => {
+        if (response.StrHoraInicio) {
+          const responseInitialTime = parseInt(
+            response.StrHoraInicio.slice(0, 2)
+          )
+          if (responseInitialTime < time) {
+            time = responseInitialTime
+          }
+        }
+      })
+      return time
+    },
+    finalTime() {
+      let time = 20
+      this.responses.forEach(response => {
+        if (response.StrHoraFin) {
+          const responseFinalTime = parseInt(response.StrHoraFin.slice(0, 2))
+          if (responseFinalTime > time) {
+            time = responseFinalTime
+          }
+        }
+      })
+      return time
+    },
+  },
+  methods: {
+    async fetchProvider(provider, date) {
+      const response = await fetch(`${apiURL}/${provider}/${date}`)
+      const text = await response.json()
+      if (text.d) {
+        this.responses.push(text.d)
+      }
+    },
+  },
+  watch: {
+    pickedDate: function(value) {
+      this.responses = []
+      this.fetchProvider('conecta', value)
+    },
+  },
 }
 </script>
