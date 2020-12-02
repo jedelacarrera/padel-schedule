fa5dbeb9e9a44f34067ecca712a1a49fff9fc63d
jedelacarrera <jedelacarrera@uc.cl>
2020-12-02 00:51:31 -0300

    :star: get availability. Debug with colors

---
+++ front/src/apiUrls.js
@@ -0,0 +1,6 @@
+const apiURL = 'https://padel-api-54td4ousva-uc.a.run.app/'
+// const apiURL = 'http://localhost:8000/'
+
+export const getSchedule = apiURL + 'get_schedule'
+export const getAvailability = apiURL + 'get_availability'
+export const getFixedTimeInfo = apiURL + 'get_fixed_time_info'
--- front/src/providers.js
+++ front/src/providers.js
@@ -1,6 +1,8 @@
 export default {
-  Conecta: {
-    url: 'http://www.clubconecta.cl/Booking/Grid.aspx',
+  conecta: {
+    id: 'conecta',
+    name: 'Conecta',
+    url: 'http://www.clubconecta.cl/',
     prices: [
       {
         time: 'Lunes a Viernes de 9:00 a 18:00 hrs',
@@ -15,10 +17,15 @@ export default {
         price: '$20.000 por 90 minutos',
       },
     ],
+    image: 'http://www.clubconecta.cl/ClubConecta/img/logo.png',
+    background: 'black',
     address: 'Av. El Rodeo 12.699, Lo Barnechea',
+    terms: 'http://www.clubconecta.cl/Booking/files.ashx?id=138',
   },
-  'Padel Break': {
-    url: 'http://padelbreak.cl/Booking/Grid.aspx',
+  padelbreak: {
+    id: 'padelbreak',
+    name: 'Padel Break',
+    url: 'http://padelbreak.cl/',
     prices: [
       {
         time: 'Lunes a Viernes de 9:00 a 18:00 hrs',
@@ -33,11 +40,14 @@ export default {
         price: '$12.000',
       },
     ],
+    image: 'http://padelbreak.cl/PadelBreak/img/logo.png',
     address:
       'Monseñor Álvaro del Portillo 12455, Las Condes. (Universidad de los Andes)',
   },
-  Santuario: {
-    url: 'https://www.tenissantuario.cl/Booking/Grid.aspx',
+  santuario: {
+    id: 'santuario',
+    name: 'Santuario',
+    url: 'https://www.tenissantuario.cl/',
     prices: [
       {
         time: 'Lunes a Viernes de 9:00 a 18:00 hrs',
@@ -52,11 +62,15 @@ export default {
         price: '$14.000',
       },
     ],
+    image:
+      'https://www.tenissantuario.cl/TenisSantuarioDelValleNueva/img/logo.svg',
     address:
       'Av. Boulevard Jardín de los Pájaros Norte #4666, Lo Barnechea. (Santuario del Valle)',
   },
-  'Bull Padel': {
-    url: 'http://www.bullpadelcenter.cl/Booking/Grid.aspx',
+  bullpadel: {
+    id: 'bullpadel',
+    name: 'Bull Padel',
+    url: 'http://www.bullpadelcenter.cl/',
     prices: [
       {
         time: 'Lunes a Viernes hasta 18:00 hrs',
@@ -71,10 +85,14 @@ export default {
         price: '$14.000',
       },
     ],
+    image: 'http://www.bullpadelcenter.cl/BullPadelCenter/img/logo.png',
+    background: 'black',
     address: 'Avenida El Cóndor 743, Huechuraba. (Ciudad Empresarial)',
   },
-  'Padel Oriente': {
-    url: 'http://padeloriente.cl/Booking/Grid.aspx',
+  padeloriente: {
+    id: 'padeloriente',
+    name: 'Padel Oriente',
+    url: 'http://padeloriente.cl/',
     prices: [
       {
         time: 'Lunes a viernes 9 a 18, sábados y domingos 14 a 18',
@@ -85,10 +103,13 @@ export default {
         price: '$20.000',
       },
     ],
+    image: 'http://padeloriente.cl/PadelOriente/img/logo.png',
     address: 'Av. Kennedy 9351, Las Condes. (Club Palestino)',
   },
-  'Alto Padel': {
-    url: 'http://www.altopadel.cl/Booking/Grid.aspx',
+  altopadel: {
+    id: 'altopadel',
+    name: 'Alto Padel',
+    url: 'http://www.altopadel.cl/',
     prices: [
       {
         time: 'Lunes a viernes 9 a 18, sábados y domingos 14 a 16',
@@ -107,10 +128,13 @@ export default {
         price: '$14.000',
       },
     ],
+    image: 'http://www.altopadel.cl/AltoPadel/img/logo.png',
     address: 'Avenida Las Condes 11755, Las Condes (Corfo)',
   },
-  'Más Padel': {
-    url: 'http://www.maspadel.cl/Booking/Grid.aspx',
+  maspadel: {
+    id: 'maspadel',
+    name: 'Más Padel',
+    url: 'http://www.maspadel.cl/',
     prices: [
       {
         time: 'Lunes a viernes hasta las 18 hrs',
@@ -125,10 +149,13 @@ export default {
         price: '$16.000',
       },
     ],
+    image: 'http://www.maspadel.cl/images.ashx?cmd=get&id=26',
     address: 'Camino San Francisco de Asis 199, Las Condes (Club Pato Cornejo)',
   },
-  'Club Rinconada': {
-    url: 'http://clubrinconada.cl/Booking/Grid.aspx',
+  rinconada: {
+    id: 'rinconada',
+    name: 'Club Rinconada',
+    url: 'http://clubrinconada.cl/',
     prices: [
       {
         time: 'Lunes a viernes 7 a 18 hrs',
@@ -143,6 +170,7 @@ export default {
         price: '$14.000',
       },
     ],
+    image: 'http://clubrinconada.cl/images.ashx?cmd=get&id=65',
     address: 'Rinconada El Salto 535, Huechuraba (Ciudad Empresarial)',
   },
 }
